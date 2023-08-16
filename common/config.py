386a5a58ffed10605aba2bcf129095b763d31a36
ZEEE <j1nd2su@gmail.com>
2023-08-17 06:36:34 +0900

    chore: modified the contents of .gitignore

---
+++ common/config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://api.vrchat.cloud/api/1"
+USER_AGENT = "VRC-Friend-Manager/1.0"
+DENY_FILE_PATH = "deny.txt"
+GLOBAL_HEADERS = {
+    'User-Agent': USER_AGENT
+}
+TOKEN = None
+DENY_USER = []
+USER_DATA = []
